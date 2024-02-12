81c1bed130a6881580b97ec5d2624c2c6e9d2157
asharcinie3 <arcinie3@msu.edu>
2024-02-12 00:08:29 -0500

    try

--- piano.js
+++ piano.js
@@ -2,17 +2,35 @@ function toggleMenu() {
     var mobileLinks = document.querySelector('.links.mobile');
     mobileLinks.style.display = (mobileLinks.style.display === 'block') ? 'none' : 'block';
 }
- 
-function showP(element) {
-    var pElement = element.querySelector('p');
-    if (pElement) {
-        pElement.style.display = 'block';
-    }
-}
 
-function hideP(element) {
-    var pElement = element.querySelector('p');
-    if (pElement) {
-        pElement.style.display = 'none';
-    }
-}
+const sound = {65:"http://carolinegabriel.com/demo/js-keyboard/sounds/040.wav",
+                87:"http://carolinegabriel.com/demo/js-keyboard/sounds/041.wav",
+                83:"http://carolinegabriel.com/demo/js-keyboard/sounds/042.wav",
+                69:"http://carolinegabriel.com/demo/js-keyboard/sounds/043.wav",
+                68:"http://carolinegabriel.com/demo/js-keyboard/sounds/044.wav",
+                70:"http://carolinegabriel.com/demo/js-keyboard/sounds/045.wav",
+                84:"http://carolinegabriel.com/demo/js-keyboard/sounds/046.wav",
+                71:"http://carolinegabriel.com/demo/js-keyboard/sounds/047.wav",
+                89:"http://carolinegabriel.com/demo/js-keyboard/sounds/048.wav",
+                72:"http://carolinegabriel.com/demo/js-keyboard/sounds/049.wav",
+                85:"http://carolinegabriel.com/demo/js-keyboard/sounds/050.wav",
+                74:"http://carolinegabriel.com/demo/js-keyboard/sounds/051.wav",
+                75:"http://carolinegabriel.com/demo/js-keyboard/sounds/052.wav",
+                79:"http://carolinegabriel.com/demo/js-keyboard/sounds/053.wav",
+                76:"http://carolinegabriel.com/demo/js-keyboard/sounds/054.wav",
+                80:"http://carolinegabriel.com/demo/js-keyboard/sounds/055.wav",
+                186:"http://carolinegabriel.com/demo/js-keyboard/sounds/056.wav"};
+
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function(event) {
+        const keyPressed = event.keyCode;
+        const keyElement = document.querySelector(`.piano-key[data-key="${keyPressed}"]`);
+        if (keyElement) {
+            keyElement.classList.add('key-pressed');
+            setTimeout(() => {
+                keyElement.classList.remove('key-pressed');
+            }, 100);
+        }
+    });
+});
