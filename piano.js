4936a566e5e35caf6af0b064ea954b756bf03125
asharcinie3 <arcinie3@msu.edu>
2024-02-12 00:17:25 -0500

    add piano func

--- piano.js
+++ piano.js
@@ -3,34 +3,53 @@ function toggleMenu() {
     mobileLinks.style.display = (mobileLinks.style.display === 'block') ? 'none' : 'block';
 }
 
-const sound = {65:"http://carolinegabriel.com/demo/js-keyboard/sounds/040.wav",
-                87:"http://carolinegabriel.com/demo/js-keyboard/sounds/041.wav",
-                83:"http://carolinegabriel.com/demo/js-keyboard/sounds/042.wav",
-                69:"http://carolinegabriel.com/demo/js-keyboard/sounds/043.wav",
-                68:"http://carolinegabriel.com/demo/js-keyboard/sounds/044.wav",
-                70:"http://carolinegabriel.com/demo/js-keyboard/sounds/045.wav",
-                84:"http://carolinegabriel.com/demo/js-keyboard/sounds/046.wav",
-                71:"http://carolinegabriel.com/demo/js-keyboard/sounds/047.wav",
-                89:"http://carolinegabriel.com/demo/js-keyboard/sounds/048.wav",
-                72:"http://carolinegabriel.com/demo/js-keyboard/sounds/049.wav",
-                85:"http://carolinegabriel.com/demo/js-keyboard/sounds/050.wav",
-                74:"http://carolinegabriel.com/demo/js-keyboard/sounds/051.wav",
-                75:"http://carolinegabriel.com/demo/js-keyboard/sounds/052.wav",
-                79:"http://carolinegabriel.com/demo/js-keyboard/sounds/053.wav",
-                76:"http://carolinegabriel.com/demo/js-keyboard/sounds/054.wav",
-                80:"http://carolinegabriel.com/demo/js-keyboard/sounds/055.wav",
-                186:"http://carolinegabriel.com/demo/js-keyboard/sounds/056.wav"};
-
- 
 document.addEventListener('DOMContentLoaded', function() {
+    const pianoKeys = document.querySelectorAll('.piano-key');
+
+    pianoKeys.forEach(function(key) {
+        key.addEventListener('mouseover', function() {
+            const keyText = this.querySelector('.key-text');
+            if (keyText) {
+                keyText.style.display = 'block';
+            }
+        });
+
+        key.addEventListener('mouseout', function() {
+            const keyText = this.querySelector('.key-text');
+            if (keyText) {
+                keyText.style.display = 'none';
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sound = {
+        65:"http://carolinegabriel.com/demo/js-keyboard/sounds/040.wav",
+        87:"http://carolinegabriel.com/demo/js-keyboard/sounds/041.wav",
+        83:"http://carolinegabriel.com/demo/js-keyboard/sounds/042.wav",
+        69:"http://carolinegabriel.com/demo/js-keyboard/sounds/043.wav",
+        68:"http://carolinegabriel.com/demo/js-keyboard/sounds/044.wav",
+        70:"http://carolinegabriel.com/demo/js-keyboard/sounds/045.wav",
+        84:"http://carolinegabriel.com/demo/js-keyboard/sounds/046.wav",
+        71:"http://carolinegabriel.com/demo/js-keyboard/sounds/047.wav",
+        89:"http://carolinegabriel.com/demo/js-keyboard/sounds/048.wav",
+        72:"http://carolinegabriel.com/demo/js-keyboard/sounds/049.wav",
+        85:"http://carolinegabriel.com/demo/js-keyboard/sounds/050.wav",
+        74:"http://carolinegabriel.com/demo/js-keyboard/sounds/051.wav",
+        75:"http://carolinegabriel.com/demo/js-keyboard/sounds/052.wav",
+        79:"http://carolinegabriel.com/demo/js-keyboard/sounds/053.wav",
+        76:"http://carolinegabriel.com/demo/js-keyboard/sounds/054.wav",
+        80:"http://carolinegabriel.com/demo/js-keyboard/sounds/055.wav",
+        186:"http://carolinegabriel.com/demo/js-keyboard/sounds/056.wav"};
+
     document.addEventListener('keydown', function(event) {
         const keyPressed = event.keyCode;
-        const keyElement = document.querySelector(`.piano-key[data-key="${keyPressed}"]`);
-        if (keyElement) {
-            keyElement.classList.add('key-pressed');
-            setTimeout(() => {
-                keyElement.classList.remove('key-pressed');
-            }, 100);
+        const soundURL = sound[keyPressed];
+        
+        if (soundURL) {
+            const audio = new Audio(soundURL);
+            audio.play();
         }
     });
 });
