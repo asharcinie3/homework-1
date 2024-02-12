839fac715bfa57714184c9c0577d92e4fafc3f75
asharcinie3 <arcinie3@msu.edu>
2024-02-11 23:03:18 -0500

    add menu mobile interactiviety

---
+++ paino.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var mobileLinks = document.querySelector('.links.mobile');
+    mobileLinks.style.display = (mobileLinks.style.display === 'block') ? 'none' : 'block';
+}
+ 
